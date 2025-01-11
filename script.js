25f8ac08cdf41f3a3a56312984aa0bad4d443783
Muhammad Qasim <qasimdogar077@gmail.com>
2025-01-11 22:48:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// Smooth scroll to sections (if necessary for future updates)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
