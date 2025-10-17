96567d538eb49e117187729b462911fa0d1ae6be
Muhammad Qasim <qasimdogar077@gmail.com>
2025-10-18 00:14:23 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll to sections (if necessary for future updates)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
